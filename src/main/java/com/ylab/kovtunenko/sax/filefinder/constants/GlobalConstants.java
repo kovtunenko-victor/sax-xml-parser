e83c3c00f28d41c87ecda50e91f7174d813fbb43
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2021-11-28 01:09:09 +0300

    Fix GlobalConstants.NEW_LINE value

--- src/main/java/com/ylab/kovtunenko/sax/filefinder/constants/GlobalConstants.java
+++ src/main/java/com/ylab/kovtunenko/sax/filefinder/constants/GlobalConstants.java
@@ -4,5 +4,5 @@ public interface GlobalConstants {
     public static final String SLASH = "/";
     public static final String EMPTY_STRING = "";
     public static final String NULL_STRING = "null";
-    public static final String NEW_LINE = "\r\n";
+    public static final String NEW_LINE = System.lineSeparator();
 }
